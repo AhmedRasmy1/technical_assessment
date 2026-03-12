1f99add4f9ab51555dc5a5e25fb55b533c1132b9
Ahmed Hisham <ahmedhishamrasmy1@gmail.com>
2026-03-12 19:01:24 +0200

    feat: refactor restaurant food menu layout and extract menu grid widget

--- lib/feature/home/presentation/sections/restaurant_foodmenu.dart
+++ lib/feature/home/presentation/sections/restaurant_foodmenu.dart
@@ -1,32 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:technical_assessment/feature/home/presentation/widgets/menu_grid_widget.dart';
 import '../../../../core/functions/extensions.dart';
 import '../../../../core/resources/color_manager.dart';
 import '../../../../core/resources/font_manager.dart';
 import '../../../../core/resources/string_manager.dart';
 import '../../../../core/widgets/custom_section_title.dart';
-import '../../data/restaurant_foodmenu/foodmenu_data.dart';
-import 'food_menu_card.dart';
 
 class RestaurantFoodmenu extends StatelessWidget {
   const RestaurantFoodmenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    List<Widget> leftColumn = [];
-    List<Widget> rightColumn = [];
-
-    for (int i = 0; i < FoodmenuData.foodmenuList.length; i++) {
-      var item = FoodmenuData.foodmenuList[i];
-      var cardWidget = Padding(
-        padding: EdgeInsets.only(bottom: context.screenHeight * 0.02),
-        child: FoodMenuCard(model: item),
-      );
-      if (i % 2 == 0) {
-        leftColumn.add(cardWidget);
-      } else {
-        rightColumn.add(cardWidget);
-      }
-    }
     return Align(
       alignment: Alignment.topLeft,
       child: Column(
@@ -39,14 +23,7 @@ class RestaurantFoodmenu extends StatelessWidget {
             fontSize: FontSize.s15,
           ),
           SizedBox(height: context.screenHeight * 0.02),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(child: Column(children: leftColumn)),
-              SizedBox(width: context.screenWidth * 0.04),
-              Expanded(child: Column(children: rightColumn)),
-            ],
-          ),
+          buildMenuGrid(context),
         ],
       ),
     );
---
+++ lib/feature/home/presentation/widgets/menu_grid_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:technical_assessment/core/functions/extensions.dart';
+import 'package:technical_assessment/feature/home/data/restaurant_foodmenu/foodmenu_data.dart';
+import 'package:technical_assessment/feature/home/presentation/sections/food_menu_card.dart';
+
+Widget buildMenuGrid(BuildContext context) {
+  List<Widget> leftColumn = [];
+  List<Widget> rightColumn = [];
+
+  for (int i = 0; i < FoodmenuData.foodmenuList.length; i++) {
+    var item = FoodmenuData.foodmenuList[i];
+    var cardWidget = Padding(
+      padding: EdgeInsets.only(bottom: context.screenHeight * 0.02),
+      child: FoodMenuCard(model: item),
+    );
+    if (i % 2 == 0) {
+      leftColumn.add(cardWidget);
+    } else {
+      rightColumn.add(cardWidget);
+    }
+  }
+
+  return Row(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Expanded(child: Column(children: leftColumn)),
+      SizedBox(width: context.screenWidth * 0.04),
+      Expanded(child: Column(children: rightColumn)),
+    ],
+  );
+}
